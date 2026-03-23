532ab31731cd1deddd947e478356b88f520a4216
HexCipher224 <mofiamadiback@gmail.com>
2026-03-23 22:04:57 +0300

    add index.js content

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
+function calculateTax(amount){
+    return amount * (0.1);
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+
+function findMaximum(num1 , num2) {
+    if (num1 > num2) {
+        return (num1);
+    }
+    else{
+        return (num2);
+    }
+}
+
+function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice , discountPercentage) {
+    const discount = originalPrice * (20/100);
+    return amount = originalPrice - discount;
+}
+
+console.log(calculateTax("500"));
+console.log(convertToUpperCase("madam"));
+console.log(findMaximum("7" , "10"));
+console.log(isPalindrome("mango"));
+console.log(calculateDiscountedPrice("1000"));
 
 
 
